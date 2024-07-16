11c80455a767483c423b69a1fbb01dd4da69cc65
Jonnathan23 <jsaquicela26@gmail.com>
2024-07-16 07:16:22 -0500

    Inicial commit

---
+++ pruebaSaquicelaJonnathan/src/main/java/prueba/pruebaSaquicelaJonnathan/Inicio.java
@@ -0,0 +1,15 @@
+package prueba.pruebaSaquicelaJonnathan;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+
+@Singleton
+@Startup
+public class Inicio {
+
+	@PostConstruct
+	public void init() {
+		
+	}
+}
